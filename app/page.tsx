dfabfe55baf03d761e21d8bacc699de0287290f8
Matheos Profirescu <matheosprofirescu@yahoo.com>
2023-07-17 13:16:26 +0300

    wiki search app done

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className={inter.className}>
-      <h1 className='text-red-500 text-4xl'>Hello</h1>
     </div>
   )
 }
